15ed3d049317b0559f446aae5ebe36f00cfd0371
Cristian Ospina <eydonista@gmail.com>
2021-12-17 09:26:25 -0500

    installed eslint

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        cabecerita
+      </header>
+      <main>
+        cuerpito
+      </main>
+    </div>
+  );
+}
+
+export default App;
